10685f49e360740224a3dc58053df8134f197846
ryoNagata001 <ryo.nagata.utokyo@gmail.com>
2017-02-17 17:29:25 +0900

    Add files via upload

---
+++ keijiban/comments/update.create.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if($_POST['token'] != session_id()){
+    echo "正規のルートから編集を行ってください";
+    exit;
+}
+$new_content = $_POST['new_content'];
+$comment_id = $_POST['comment_id'];
+
+try{
+    require_once('../pdo.php');
+}catch(PDOException $e){
+    var_dump($e->getMessage());
+    exit;
+}
+
+$stmt = $db->prepare("update comments set content = :content where id = :id");
+$stmt->execute([
+    ':content'=>$new_content,
+    ':id'=>$comment_id
+]);
+
+echo "編集しました<br>";
+echo '<a href="../threads/show_all.php">スレッド一覧に戻る</a>';
